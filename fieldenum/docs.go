11bac5b7355a38c2c473ed122d3d1cd87e54de1d
QAQ <m13154397827@163.com>
2025-11-30 19:02:26 +0800

    docs(fieldenum): update docs

--- fieldenum/docs.go
+++ fieldenum/docs.go
@@ -85,26 +85,26 @@ The expression system uses three numeric types:
 
 # Literals
 
-- Integers: 123, 0xFF, 0b10101, 0123
-- Floats: 123.456, .123, 1.23e10, 1.23E10
-- Complex: 123.456i, .123i, 1.23e10i, 1.23E10i
+  - Integers: 123, 0xFF, 0b10101, 0123
+  - Floats: 123.456, .123, 1.23e10, 1.23E10
+  - Complex: 123.456i, .123i, 1.23e10i, 1.23E10i
 
 # Built-in Constants
 
-- i = 0i
-- e = math.E
-- pi|Pi = math.Pi
-- phi|Phi = math.Phi
-- nan|NaN = math.NaN()
-- inf|Inf = math.Inf(1)
-- iota = current field index (int64)
+  - i = 0i
+  - e = math.E
+  - pi|Pi = math.Pi
+  - phi|Phi = math.Phi
+  - nan|NaN = math.NaN()
+  - inf|Inf = math.Inf(1)
+  - iota = current field index (int64)
 
 # Operators
 
 See package documentation for detailed operator tables covering:
-- Unary: +, -, ^
-- Binary: +, -, *, /, %, &, |, ^, <<, >>, &^
-- Type promotion and overflow handling rules
+  - Unary: +, -, ^
+  - Binary: +, -, *, /, %, &, |, ^, <<, >>, &^
+  - Type promotion and overflow handling rules
 
 Unary Operators:
 
@@ -160,10 +160,10 @@ Binary Operators:
 
 # Built-in Functions
 
-- Type conversion: int(), float(), complex(), real(), imag()
-- Math: abs(), sqrt(), pow(), exp(), log(), log10()
-- Trigonometry: sin(), cos(), tan(), asin(), acos(), atan(), sinh(), cosh(), tanh(), asinh(), acosh(), atanh()
-- Aggregation: max(), min()
+  - Type conversion: int(), float(), complex(), real(), imag()
+  - Math: abs(), sqrt(), pow(), exp(), log(), log10()
+  - Trigonometry: sin(), cos(), tan(), asin(), acos(), atan(), sinh(), cosh(), tanh(), asinh(), acosh(), atanh()
+  - Aggregation: max(), min()
 
 Type Conversion Functions:
 
